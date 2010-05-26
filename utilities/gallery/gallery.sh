265939aa2ff07824c7c1915578a062906bcbaea9
Harish Narayanan <harish@simula.no>
2010-05-26 09:00:52 +0200

    Updated gallery generator to randomize image order

--- utilities/gallery/gallery.sh
+++ utilities/gallery/gallery.sh
@@ -6,7 +6,10 @@ cat gallery-header.html > ${output_file}
 
 for file in ${1}/*.jpg
 do
-    echo '	          <a href="<!--#echo var="MY_DOCUMENT_ROOT"-->/files/images/photos/'${file}'" title=""><img class="thumbnail-photo" src="<!--#echo var="MY_DOCUMENT_ROOT"-->/files/images/photos/'`dirname ${file}`'/thumbnails/'`basename ${file}`'" alt="<!--#echo var="pagetitle" -->" /></a>' >> ${output_file}
+    echo '	          <a href="<!--#echo var="MY_DOCUMENT_ROOT"-->/files/images/photos/'${file}'" title=""><img class="thumbnail-photo" src="<!--#echo var="MY_DOCUMENT_ROOT"-->/files/images/photos/'`dirname ${file}`'/thumbnails/'`basename ${file}`'" alt="<!--#echo var="pagetitle" -->" /></a>' >> ${output_file}_tmp
 done
+
+sort -R ${output_file}_tmp >> ${output_file}
+rm ${output_file}_tmp
 
 cat gallery-footer.html >> ${output_file}
