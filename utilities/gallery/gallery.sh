9e19d44737408fdf4ead89fbc32ca25720669563
Harish Narayanan <harish@simula.no>
2012-01-31 19:41:50 +0100

    Removed randomisation in gallery generation

--- utilities/gallery/gallery.sh
+++ utilities/gallery/gallery.sh
@@ -9,7 +9,7 @@ do
     echo '	          <a href="<!--#echo var="MY_DOCUMENT_ROOT"-->/files/images/photos/'${file}'" title=""><img class="thumbnail-photo" src="<!--#echo var="MY_DOCUMENT_ROOT"-->/files/images/photos/'`dirname ${file}`'/thumbnails/'`basename ${file}`'" alt="<!--#echo var="pagetitle" -->" /></a>' >> ${output_file}_tmp
 done
 
-sort -R ${output_file}_tmp >> ${output_file}
+sort ${output_file}_tmp >> ${output_file}
 rm ${output_file}_tmp
 
 cat gallery-footer.html >> ${output_file}
